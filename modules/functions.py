a98032ff1854951a600722a567f052d0c351bd3c
Ricardo Torres <rtorres@oesia.com>
2023-10-25 12:38:43 +0200

    Finalize_principal_process_ tested

--- modules/functions.py
+++ modules/functions.py
@@ -2,12 +2,18 @@ import json, os, glob
 
 # region Constants
 
+# Public constants
+ConsYNotFound = "POS_Y no database"
+
+# Private constants
+_consCountForFinde = 4
 _consQuery = "INSERT INTO SGA_SILO_SHUTTLE.POSICION_REFERENCIA_PLC (ID_POSICION,ID_INSTALACION,REFERENCIA_PLC) VALUES "
 _consFileExtension = ".sql"
 
 # endregion
 
 lstParameters = []
+dictionaryLanes = {100: 23, 200: 23, 300: 28}
 
 
 def queryParameterJson(pathFile):
@@ -52,26 +58,79 @@ def generateFile(asileInt, pathOutputFolder, lstLines):
     f.close()
 
 
-def queryPositionJson(*args):
+# lstX_compare = It is a complex structure because only this type of structure
+# can be passed by reference.
+def queryPositionJson(*args, lstX_compare):
+    idPosicion = str()
+    totalIterations = 0
+
     if args and len(args[0]) == 6:
+        # path JSON file
         pathfile = str(args[0][0]).strip()
+        # Only the pos_X is changed to compare if the height is equal to 1
+        isY1Compare = True if (args[0][4]) == 1 else False
+
         with open(pathfile) as file:
             data = json.load(file)
 
-            result = [
-                x
-                for x in data
-                if x["ID_PASILLO"] == args[0][1]
-                and x["LADO"] == args[0][2]
-                and x["POSICION_X"] == args[0][3]
-                and x["POSICION_Y"] == args[0][4]
-                and x["POSICION_Z"] == args[0][5]
-            ]
+            # Until it find a position
+            while not idPosicion and totalIterations < _consCountForFinde:
+                result = queryJson(
+                    data,
+                    args[0][1],
+                    args[0][2],
+                    args[0][3] + lstX_compare[0],
+                    args[0][4],
+                    args[0][5],
+                )
 
-        file.close
+                idPosicion = str(result[0]["ID_POSICION"]) if len(result) > 0 else str()
 
-        idPosicion = str(result[0]["ID_POSICION"]) if len(result) > 0 else ""
+                # If no position is found and the query is about the first height (Y=1), the next pos_X will be searched for.
+                if not idPosicion:
+                    if isY1Compare:
+                        increaseListValue = lstX_compare[0] + 1
+                        lstX_compare.clear()
+                        lstX_compare.append(increaseListValue)
+
+                        totalIterations += 1
+                    else:
+                        # Value is assigned to exit the loop
+                        idPosicion = ConsYNotFound
+
+            file.close
     else:
-        idPosicion = ""
+        idPosicion = str()
 
     return idPosicion
+
+
+def queryJson(jsonfile, idPasillo, lado, posX, posY, posZ):
+   
+    result = [
+        x
+        for x in jsonfile
+        if x["ID_PASILLO"] == idPasillo
+        and x["LADO"] == lado
+        and x["POSICION_X"] == posX
+        and x["POSICION_Y"] == posY
+        and x["POSICION_Z"] == posZ
+    ]
+
+    return result
+
+
+def isChangeLane(laneCount, posXDatabase):
+    changeLaneValue = dictionaryLanes.get(laneCount)
+    if posXDatabase == changeLaneValue:
+        return True
+    else:
+        return False
+
+
+def rebootDisableXList():
+    posXDisabledCount = []
+    posXDisabledCount.clear()
+    posXDisabledCount.append(0)
+
+    return posXDisabledCount
