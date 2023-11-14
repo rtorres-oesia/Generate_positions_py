8094366f1e608c6778d707f8842659693acecce3
Ricardo Torres <rtorres@oesia.com>
2023-11-14 15:47:26 +0100

    Load JSON Position in memory once time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json, glob, os
 import modules.functions as fn
 from os.path import exists
 
@@ -16,7 +17,7 @@ columns_for_query = lstParameters[4]
 
 # Constants
 _consHeaderLoc = "LocationCode"
-_consIdInstalacion = "38"
+_consIdInstalacion = "27695"
 
 # Parameter class
 isContinue = True
@@ -39,201 +40,217 @@ lstPositions = []
 
 # Check files exists
 if not exists(pathJSON) or not exists(pathExcelLocation):
-    isContinue = False
+	isContinue = False
 
 if isContinue:
-    # Output folder management
-    isContinue = fn.createFolder(pathOutputFolder)
+	# Output folder management
+	isContinue = fn.createFolder(pathOutputFolder)
 else:
-    print("Files for loaded not exists!")
+	print("Files for loaded not exists!")
 
 # Algorithm locations
 if isContinue:
-    # region Excel management
+	# region Excel management
 
-    # Excel in memory
-    # Get two columns in sheet
-    df = pd.read_excel(
-        pathExcelLocation,
-        sheet_name=sheet_name_param,
-        index_col=None,
-        usecols=columns_for_query,
-    )
+	# Excel in memory
+	# Get two columns in sheet
+	df = pd.read_excel(
+		pathExcelLocation,
+		sheet_name=sheet_name_param,
+		index_col=None,
+		usecols=columns_for_query,
+	)
 
-    # Get total rows in Excel; Does not take header into account
-    row_cont = len(df.index)
-    print("Total rows: " + str(row_cont))
+	# Get total rows in Excel; Does not take header into account
+	row_cont = len(df.index)
+	print("Total rows: " + str(row_cont))
 
-    # Get first aisle&lane before Excel loop
-    aisle = int(str(df.iloc[1][_consHeaderLoc]).split("-")[1])
+	# Get first aisle&lane before Excel loop
+	aisle = int(str(df.iloc[1][_consHeaderLoc]).split("-")[1])
 
-    # Initialize aisle & Lane
-    # Ex: Aisle 1 - Lane 1 = 101; aisle 1 - Lane 2 = 201; Aisle 2 - Lane 1 =
-    laneCount = 100
-    # First aisle for query
-    aisleQuery = 1
+	# Initialize aisle & Lane
+	# Ex: Aisle 1 - Lane 1 = 101; aisle 1 - Lane 2 = 201; Aisle 2 - Lane 1 =
+	laneCount = 100
+	# First aisle for query
+	aisleQuery = 1
 
-    # Initialize counter for X positions that do not exist in the ITX database
-    posXDisabledCount = fn.rebootDisableXList()
-    posXDisabledCompare = 0
+	# Initialize counter for X positions that do not exist in the ITX database
+	posXDisabledCount = fn.rebootDisableXList()
+	posXDisabledCompare = 0
 
-    # Loop excel
-    for x in range(row_cont):
-        # To display row information and generate file
-        location = str(df.iloc[x][_consHeaderLoc]).strip()
+	# Open JSON File
+	with open(pathJSON) as file:
+		data = json.load(file)
 
-        # To use in the process
-        loc_value = location.split("-")
+		# Loop excel
+		for x in range(row_cont):
+			# To display row information and generate file
+			location = str(df.iloc[x][_consHeaderLoc]).strip()
 
-        # For errors logs
-        rowExcelCount += 1
+			if location == 'HAI_002-005-05_1':
+				print("Posicion")
+			
+			# To use in the process
+			loc_value = location.split("-")
 
-        # Check if the retrieved fields are not empty and the positions array have enough information
-        if loc_value[0].split() and len(loc_value) == 4:
-            # Recovery initial aisle and check if there is change to the following
-            aisle_value = int(loc_value[1])
-            if aisle != aisle_value:
-                aisle = aisle_value
-                posXDisabledCount = fn.rebootDisableXList()
-                posXDisabledCompare = 0
+			# For errors logs
+			rowExcelCount += 1
 
-                # You can only change aisles when you have traveled both sides of each aisle
-                if aisle_value % 2 != 0:
-                    aisleQuery += 1
-                    if len(lstPositions) > 0:
-                        # Generate file with aisle values
-                        fn.generateFile(
-                            str(finalAisle)[-2:], pathOutputFolder, lstPositions
-                        )
-                        # Variable reboot for save positions
-                        lstPositions = []
+			# Check if the retrieved fields are not empty and the positions array have enough information
+			if loc_value[0].split() and len(loc_value) == 4:
+				# Recovery initial aisle and check if there is change to the following
+				aisle_value = int(loc_value[1])
+				if aisle != aisle_value:
+					aisle = aisle_value
+					posXDisabledCount = fn.rebootDisableXList()
+					posXDisabledCompare = 0
 
-                        print("Asile: " + str(finalAisle)[-2:] + " finished!")
+					# You can only change aisles when you have traveled both sides of each aisle
+					if aisle_value % 2 != 0:
+						aisleQuery += 1
+						if len(lstPositions) > 0:
+							# Generate file with aisle values
+							fn.generateFile(
+								str(finalAisle)[-2:], pathOutputFolder, lstPositions
+							)
+							# Variable reboot for save positions
+							lstPositions = []
 
-                isChangeAisle = True
-            else:
-                isChangeAisle = False
+							print("Asile: " + str(finalAisle)[-2:] + " finished!")
 
-            # Get POS_Y
-            arrayYZ = loc_value[3].split("_")
-            if arrayYZ and len(arrayYZ) == 2:
-                finalPosY = int(arrayYZ[0])
-                finalPosZ = int(arrayYZ[1])
+					isChangeAisle = True
+				else:
+					isChangeAisle = False
 
-                isContinue = True
-            else:
-                isContinue = False
+				# Get POS_Y
+				arrayYZ = loc_value[3].split("_")
+				if arrayYZ and len(arrayYZ) == 2:
+					finalPosY = int(arrayYZ[0])
+					finalPosZ = int(arrayYZ[1])
 
-            if isContinue:
-                # Check change lane
-                # Get POS_X
-                finalPosX = int(loc_value[2])
+					isContinue = True
+				else:
+					isContinue = False
 
-                # Check the posX change to increase the counter and be able to reset it with lane changes
-                if posXInitial != finalPosX:
-                    posXInitial = finalPosX
-                    if (
-                        fn.isChangeLane(laneCount, (posXQuery + posXDisabledCompare))
-                        or isChangeAisle
-                    ):
-                        # Reboot variables
-                        posXDisabledCount = fn.rebootDisableXList()
-                        posXQuery = 1
+				if isContinue:
+					# Check change lane
+					# Get POS_X
+					finalPosX = int(loc_value[2])
 
-                        # With the lane change, the lane resets to 100
-                        laneCount = 100 if isChangeAisle else laneCount + 100
-                    else:
-                        posXQuery += 1
+					# Check the posX change to increase the counter and be able to reset it with lane changes
+					if posXInitial != finalPosX:
+						posXInitial = finalPosX
+						if (
+							fn.isChangeLane(
+								laneCount, (posXQuery + posXDisabledCompare)
+							)
+							or isChangeAisle
+						):
+							# Reboot variables
+							posXDisabledCount = fn.rebootDisableXList()
+							posXQuery = 1
 
-                # Change real aisle value for query in table dbo.POSICION
-                finalAisle = laneCount + aisleQuery
+							# With the lane change, the lane resets to 100
+							laneCount = 100 if isChangeAisle else laneCount + 100
+						else:
+							posXQuery += 1
 
-                # Get aisle foreach iteration for calculate side
-                finalSide = 2 if aisle_value % 2 == 0 else 1
+					# Change real aisle value for query in table dbo.POSICION
+					finalAisle = laneCount + aisleQuery
 
-                # region Json Query
-                finalAisleSTR = str(finalAisle)
-                idPosicion = fn.queryPositionJson(
-                    [
-                        pathJSON,
-                        int(finalAisleSTR),
-                        # int(finalAisleSTR[-2:]),
-                        finalSide,
-                        # finalPosX,
-                        posXQuery,
-                        finalPosY,
-                        finalPosZ,
-                    ],
-                    lstX_compare=posXDisabledCount,
-                )
+					# Get aisle foreach iteration for calculate side
+					finalSide = 2 if aisle_value % 2 == 0 else 1
 
-                posXDisabledCompare = int(posXDisabledCount[0])
+					# region Json Query
+					finalAisleSTR = str(finalAisle)
+					idPosicion = fn.queryPositionJson(
+						[
+							# pathJSON,
+							data,
+							int(finalAisleSTR),
+							# int(finalAisleSTR[-2:]),
+							finalSide,
+							posXQuery,
+							finalPosY,
+							finalPosZ,
+							# provisional si hay que marcar en el JSON las que se van tratando
+							"C:/WorkPlace/Phyton/final.json",
+						],
+						lstX_compare=posXDisabledCount,
+					)
 
-                # It is verified that the position is informed and
-                # has not been an error generated by not finding a specific height
-                if idPosicion and idPosicion != fn.ConsYNotFound:
-                    query = str(
-                        "("
-                        + idPosicion
-                        + ","
-                        + _consIdInstalacion
-                        + ",'"
-                        + location
-                        + "')"
-                    )
+					posXDisabledCompare = int(posXDisabledCount[0])
 
-                    lstPositions.append(query)
+					# It is verified that the position is informed and
+					# has not been an error generated by not finding a specific height
+					if idPosicion and idPosicion != fn.ConsYNotFound:
+						query = str(
+							"("
+							+ idPosicion
+							+ ","
+							+ _consIdInstalacion
+							+ ",'"
+							+ location
+							+ "')"
+						)
 
-                    print(
-                        "Processing! idPosicion: "
-                        + idPosicion
-                        + " Excel_Loc: "
-                        + location
-                        + " Aisle: "
-                        + str(finalAisle)
-                        + " Side: "
-                        + str(finalSide)
-                        + " Lane: "
-                        + str(laneCount)
-                        + " PosXQuery: "
-                        + str(posXQuery)
-                    )
-                else:
-                    print(
-                        "Location: "
-                        + location
-                        + " not found in query to dbo.Position. Excel row: "
-                        + str(rowExcelCount)
-                    )
+						lstPositions.append(query)
 
-                # endregion
+						print(
+							"Processing! idPosicion: "
+							+ idPosicion
+							+ " Excel_Loc: "
+							+ location
+							+ " Aisle: "
+							+ str(finalAisle)
+							+ " Side: "
+							+ str(finalSide)
+							+ " Lane: "
+							+ str(laneCount)
+							+ " PosXQuery: "
+							+ str(posXQuery)
+						)
+					else:
+						print(
+							"Location: "
+							+ location
+							+ " not found in query to dbo.Position. Excel row: "
+							+ str(rowExcelCount)
+						)
 
-            else:
-                print(
-                    "Error retrieving location information "
-                    + location
-                    + " in row number: "
-                    + str(rowExcelCount)
-                )
+					# endregion
 
-        # Print error
-        else:
-            print(
-                "Error to recovery Excel value. In row number: "
-                + str(rowExcelCount)
-                + " Location value: "
-                + location
-            )
+				else:
+					print(
+						"Error retrieving location information "
+						+ location
+						+ " in row number: "
+						+ str(rowExcelCount)
+					)
 
-    # Generate file with final aisle values
-    if len(lstPositions) > 0:
-        # fn.generateFile(str(finalAisle), pathOutputFolder, lstPositions)
-        fn.generateFile(str(finalAisle)[-2:], pathOutputFolder, lstPositions)
-        # Variable reboot for save positions
-        lstPositions = []
+			# Print error
+			else:
+				print(
+					"Error to recovery Excel value. In row number: "
+					+ str(rowExcelCount)
+					+ " Location value: "
+					+ location
+				)
 
-    print("Process succeed! Total positions in loop from Excel: " + str(rowExcelCount))
+		# Generate file with final aisle values
+		if len(lstPositions) > 0:
+			# fn.generateFile(str(finalAisle), pathOutputFolder, lstPositions)
+			fn.generateFile(str(finalAisle)[-2:], pathOutputFolder, lstPositions)
+			# Variable reboot for save positions
+			lstPositions = []
 
-    # endregion
+		print(
+			"Process succeed! Total positions in loop from Excel: " + str(rowExcelCount)
+		)
+
+	file.close
+	print("JSON Positions file closed!")
+
+	# endregion
 else:
-    print("Error: One or more of the files to be processed does not exist!")
+	print("Error: One or more of the files to be processed does not exist!")
--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,5 @@
 import json, os, glob
+from os.path import exists
 
 # region Constants
 
@@ -7,7 +8,7 @@ ConsYNotFound = "POS_Y no database"
 
 # Private constants
 _consCountForFinde = 4
-_consQuery = "INSERT INTO SGA_SILO_SHUTTLE.POSICION_REFERENCIA_PLC (ID_POSICION,ID_INSTALACION,REFERENCIA_PLC) VALUES "
+_consQuery = "INSERT INTO SGA_SILO_SHUTTLE_OYSHO.POSICION_REFERENCIA_PLC (ID_POSICION,ID_INSTALACION,REFERENCIA_PLC) VALUES\n"
 _consFileExtension = ".sql"
 
 # endregion
@@ -54,8 +55,26 @@ def createFolder(pathFolder):
 def generateFile(asileInt, pathOutputFolder, lstLines):
     pathfile = pathOutputFolder + "aisle_" + str(int(asileInt)) + _consFileExtension
     with open(pathfile, "w") as f:
+        # Concat query const with new values for aisle
         f.write(_consQuery + (",".join(lstLines)))
     f.close()
+
+    if exists(pathfile):
+        # Read in the file for replace carry return forach values
+        with open(pathfile, "r") as file:
+            # Load file in memory
+            fileReplace = file.read()
+        file.close
+
+        #Replace file with new values for add return carry
+        newfile=fileReplace.replace("),", "),\n")
+        
+        # Write the file out again
+        with open(pathfile, "w") as file:
+            file.write(newfile)
+        file.close
+    else:
+        print("The file: " + pathfile + " has been deleted!")
 
 
 # lstX_compare = It is a complex structure because only this type of structure
@@ -64,41 +83,54 @@ def queryPositionJson(*args, lstX_compare):
     idPosicion = str()
     totalIterations = 0
 
-    if args and len(args[0]) == 6:
+    if args and len(args[0]) == 7:
         # path JSON file
-        pathfile = str(args[0][0]).strip()
+        # pathfile = str(args[0][0]).strip()
+        pathFinalFile = str(args[0][6]).strip()
+        data = args[0][0]
         # Only the pos_X is changed to compare if the height is equal to 1
         isY1Compare = True if (args[0][4]) == 1 else False
 
-        with open(pathfile) as file:
-            data = json.load(file)
+        # Until it find a position
+        while not idPosicion and totalIterations < _consCountForFinde:
+            result = queryJson(
+                data,
+                args[0][1],
+                args[0][2],
+                args[0][3] + lstX_compare[0],
+                args[0][4],
+                args[0][5],
+            )
 
-            # Until it find a position
-            while not idPosicion and totalIterations < _consCountForFinde:
-                result = queryJson(
-                    data,
-                    args[0][1],
-                    args[0][2],
-                    args[0][3] + lstX_compare[0],
-                    args[0][4],
-                    args[0][5],
-                )
+            idPosicion = str(result[0]["ID_POSICION"]) if len(result) > 0 else str()
 
-                idPosicion = str(result[0]["ID_POSICION"]) if len(result) > 0 else str()
+            # If no position is found and the query is about the first height (Y=1), the next pos_X will be searched for.
+            if not idPosicion:
+                if isY1Compare:
+                    increaseListValue = lstX_compare[0] + 1
+                    lstX_compare.clear()
+                    lstX_compare.append(increaseListValue)
 
-                # If no position is found and the query is about the first height (Y=1), the next pos_X will be searched for.
-                if not idPosicion:
-                    if isY1Compare:
-                        increaseListValue = lstX_compare[0] + 1
-                        lstX_compare.clear()
-                        lstX_compare.append(increaseListValue)
+                    totalIterations += 1
+                else:
+                    # Value is assigned to exit the loop
+                    idPosicion = ConsYNotFound
 
-                        totalIterations += 1
-                    else:
-                        # Value is assigned to exit the loop
-                        idPosicion = ConsYNotFound
+            # ISRECORDED change in JSON file
+            # if idPosicion:
+            #    for x in data:
+            #        if x["ID_POSICION"] == int(idPosicion):
+            #            x["ISRECORDED"] = 1
+            #            break
 
-            file.close
+            #    newData =  json.dumps(data, indent=4)
+
+            #    with open(pathFinalFile, 'w') as fileUpdate:
+            #     fileUpdate.write(newData)
+
+            #     fileUpdate.close
+
+            # file.close
     else:
         idPosicion = str()
 
@@ -106,7 +138,6 @@ def queryPositionJson(*args, lstX_compare):
 
 
 def queryJson(jsonfile, idPasillo, lado, posX, posY, posZ):
-   
     result = [
         x
         for x in jsonfile
